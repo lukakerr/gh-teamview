c09ab09c5ec8f32d8a338a3392a8f8e40be19d70
lukakerr <lukakerr1@gmail.com>
2021-07-10 13:51:39 +1000

    fix

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const path = require('path');
 const express = require('express');
-const webpack = require('webpack');
-const config = require('./webpack.config.js');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
 const historyApiFallback = require('connect-history-api-fallback');
 
 const app = express();
-const compiler = webpack(config);
 
 const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || 8080;
@@ -18,23 +13,33 @@ app.use(historyApiFallback({
   verbose: false
 }));
 
-app.use(webpackDevMiddleware(compiler, {
-  noInfo: true,
-  watchOptions: {
-    aggregateTimeout: 300,
-    poll: 1000
-  },
-  stats: {
-    assets: true,
-    colors: true,
-    chunks: true,
-    children: false
-  },
-  historyApiFallback: true,
-  disableHostCheck: true
-}));
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack');
+  const config = require('./webpack.config.js');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
 
-app.use(webpackHotMiddleware(compiler));
+  const compiler = webpack(config);
+
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    },
+    stats: {
+      assets: true,
+      colors: true,
+      chunks: true,
+      children: false
+    },
+    historyApiFallback: true,
+    disableHostCheck: true
+  }));
+
+  app.use(webpackHotMiddleware(compiler));
+}
+
 app.use(express.static(DIST));
 
 app.listen(PORT, HOST, (err) => {
